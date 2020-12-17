cbc3ab4cf2f496d70fc3fe6b1ca3891a5930b855
Meghana Gudivada <meghanagudivada1999@gmail.com>
2020-12-17 12:28:16 +0530

    MVVM updates

--- app/src/main/java/com/example/recipes/view/MainActivity.kt
+++ app/src/main/java/com/example/recipes/view/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.recipes.*
 import com.example.recipes.model.ApiService
@@ -22,10 +23,15 @@ class MainActivity : AppCompatActivity()
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var selectorFragment: Fragment
     var cartItem = arrayListOf<Recipe>()
-   private lateinit var retService: ApiService
-   private lateinit var recipe: ArrayList<Recipe>
-   private lateinit var adapter: ListAdapter
+    private lateinit var recipe: ArrayList<Recipe>
+    private lateinit var listadapter: ListAdapter
     private lateinit var viewModel:MainActivityViewModel
+    private val recipeDataObserver = Observer<ArrayList<Recipe>>{
+          recipe = it
+    }
+    private val cartDataObserver = Observer<ArrayList<Recipe>>{
+        cartItem = it
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -34,58 +40,32 @@ class MainActivity : AppCompatActivity()
         setContentView(R.layout.activity_main)
         Log.i("tag","on create called")
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-       // recipe = viewModel.getData() as ArrayList<Recipe>
-        retService = RetrofitInstance.getService().create(ApiService::class.java)
-        recipe = arrayListOf()
-        Log.i("tag","on create called")
-        val call: retrofit2.Call<List<Recipe>> = retService.getRecipes()
-        call.enqueue(object : retrofit2.Callback<List<Recipe>> {
-            override fun onResponse(
-                call: retrofit2.Call<List<Recipe>>,
-                response: Response<List<Recipe>>
-            ) {
-                recipe = (response.body() as ArrayList<Recipe>?)!!
-
-                for (i in recipe) {
-                    i.isClicked = false
-                    i.num = 0
-                }
-
-
-            }
-
-            override fun onFailure(call: retrofit2.Call<List<Recipe>>, t: Throwable) {
-                Log.i("error", t.message.toString())
-            }
-
-        })
-//        https://github.com/meghanagudivada/RecipeCart
+        viewModel.recipes.observe(this,recipeDataObserver)
+        viewModel.cartItem.observe(this,cartDataObserver)
+        viewModel.getRecipes()
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> {
-                    //startActivity(Intent(this, HomeActivity::class.java))
-                    adapter = ListAdapter(recipe, this@MainActivity)
-                    adapter.notifyDataSetChanged()
-                    selectorFragment = HomeFragment(adapter)
+
+                    listadapter = ListAdapter(recipe, this@MainActivity)
+                    listadapter.notifyDataSetChanged()
+                    selectorFragment = HomeFragment(listadapter)
                 }
                 R.id.nav_search -> {
                     selectorFragment = SearchFragment()
-
                 }
 
                 R.id.nav_cart -> {
-                    //cartItem = adapter.getSelectedItems() as ArrayList<Recipe>
-                    selectorFragment = CartFragment(CartAdapter(getCartItems()))
-
+                    selectorFragment = CartFragment(CartAdapter(cartItem))
                 }
 
                 R.id.nav_profile -> selectorFragment = ProfileFragment()
             }
-            viewModel.setCurrentFragment(selectorFragment)
+
             if (selectorFragment != null) {
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, viewModel.getCurrentFragment()).commit()
+                    .replace(R.id.fragment_container,selectorFragment).commit()
             }
             true
         }
@@ -107,22 +87,11 @@ class MainActivity : AppCompatActivity()
     }
 
     override fun onItemClick(position: Int, r: Recipe) {
-        recipe[position] = r
+        viewModel.updateRecipes(position,r)
+        viewModel.getCartItems()
+
     }
 
-    fun getCartItems(): ArrayList<Recipe> {
-
-        for (i in recipe)
-            if (i.isClicked) {
-
-                if (!cartItem.contains(i)) {
-                    i.num = 1
-                    cartItem.add(i)
-                }
-            }
-
-        return cartItem
-    }
 
 
 
--- app/src/main/java/com/example/recipes/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/recipes/viewmodel/MainActivityViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.recipes.viewmodel
 
 
+import android.app.Application
 import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.recipes.model.ApiService
 import com.example.recipes.model.Recipe
@@ -11,45 +14,54 @@ import com.example.recipes.view.DefaultFragment
 import retrofit2.Response
 
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(application: Application) :AndroidViewModel(application)  {
 
-//     var retService: ApiService =  RetrofitInstance.getService().create(ApiService::class.java)
-//     private var recipe: ArrayList<Recipe> = arrayListOf()
-//     var cartItem:ArrayList<Recipe> = arrayListOf()
-       var f:Fragment = DefaultFragment()
-//     private val call: retrofit2.Call<List<Recipe>> = retService.getRecipes()
-//     fun getData():List<Recipe> {
-//        call.enqueue(object : retrofit2.Callback<List<Recipe>> {
-//            override fun onResponse(
-//                call: retrofit2.Call<List<Recipe>>,
-//                response: Response<List<Recipe>> )
-//            {
-//
-//                recipe = (response.body() as ArrayList<Recipe>?)!!
-//
-//                for (i in recipe) {
-//                    Log.i("name", i.name)
-//                    i.isClicked = false
-//                    i.num = 0
-//                }
-//
-//
-//            }
-//
-//            override fun onFailure(call: retrofit2.Call<List<Recipe>>, t: Throwable) {
-//                Log.i("error", t.message.toString())
-//            }
-//
-//        })
-//
-//      return  recipe
+       val recipes by lazy { MutableLiveData<ArrayList<Recipe>>() }
+       val cartItem by lazy { MutableLiveData<ArrayList<Recipe>>() }
+
+    fun getRecipes(){
+    var retService: ApiService =  RetrofitInstance.getService().create(ApiService::class.java)
+    val call: retrofit2.Call<List<Recipe>> = retService.getRecipes()
+    call.enqueue(object : retrofit2.Callback<List<Recipe>>{
+            override fun onResponse(
+                call: retrofit2.Call<List<Recipe>>,
+                response: Response<List<Recipe>> ) {
+
+                val data = (response.body() as ArrayList<Recipe>?)!!
+                for (i in 0 until data.size) {
+                    Log.i("name", data[i].name)
+                    data[i].isClicked = false
+                    data[i].num = 0
+                }
+                recipes.value = data }
+
+            override fun onFailure(call: retrofit2.Call<List<Recipe>>, t: Throwable) {
+                Log.i("error", t.message.toString())
+                recipes.value = null
+            }
+
+        })
+
+    }
+    fun updateRecipes(position: Int, r: Recipe ){
+        recipes.value?.set(position, r)
+    }
+
+    fun getCartItems() {
+
+        cartItem.value = arrayListOf()
+        for (i in recipes.value!!)
+               if (i.isClicked)
+                   cartItem.value?.add(i)
+
+            }
+
+
+    }
+//    fun getCurrentFragment():Fragment{
+//        return f
+//    }
+//    fun setCurrentFragment(fragment: Fragment){
+//        f= fragment
 //    }
 
-
-    fun getCurrentFragment():Fragment{
-        return f
-    }
-    fun setCurrentFragment(fragment: Fragment){
-        f= fragment
-    }
-}
